5a399a8c0167a91064762de9d7dd82edf7719dd8
josé damião <j.damiao.l.p@gmail.com>
2026-07-01 16:07:21 -0300

    primeiros passos

--- basic/00-helloworld.js
+++ basic/00-helloworld.js
@@ -1,1 +1,14 @@
+//comentario simples de uma linha.
+
+/*
+comentario de
+varias linhas
+até eu fechar.
+*/
+
+let a = "hello"
+let b = "world"
+
 console.log("Hello, World")
+console.log('Hello, World')
+console.log(`${a}, ${b}`)
---
+++ basic/01-variaveis.js
@@ -0,0 +1,19 @@
+//var
+
+var A = "hello, world"
+var A = "hello, JScript"
+console.log(A)
+
+//let
+
+let B = "hello, world2"
+//let B = "hello, JScript" XXX
+B = "hello, JScript"
+console.log(B)
+
+//const
+
+const C = "hello, world3"
+//const C = "hello, JScript"  XXX
+//C = "hello, JScript" XXX
+console.log(C)
