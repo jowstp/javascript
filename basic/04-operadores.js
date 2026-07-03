273093750c3f4129351ec456c06560492f6cea69
josé damião <j.damiao.l.p@gmail.com>
2026-07-03 20:18:45 -0300

    operadores parte 1

---
+++ basic/04-operadores.js
@@ -0,0 +1,19 @@
+//operações
+
+let a = 5
+let b = 4
+
+console.log(a + b) //soma
+console.log(a - b) //subtração
+console.log(a * b) //multiplicação
+console.log(a / b) //divisão
+console.log(a % b) //modulo
+console.log(a ** b) //expoente
+
+//incremento
+a++
+console.log(a)
+
+//decremento
+b--
+console.log(b)
